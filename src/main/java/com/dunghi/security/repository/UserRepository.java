12bfca68b086d43953849ce34056c3bbeeae555c
gdunghi <gdunghi@gmail.com>
2016-11-23 01:11:52 +0700

    init

---
+++ src/main/java/com/dunghi/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dunghi.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.dunghi.model.security.User;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
